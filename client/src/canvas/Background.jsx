05439352a70efce8414f3c7a8f032bc8e3c95390
rayson <raysonyeap4c@gmail.com>
2023-05-30 10:27:33 +0800

    lower light

--- client/src/canvas/Background.jsx
+++ client/src/canvas/Background.jsx
@@ -1,8 +1,13 @@
 import React, { useRef } from 'react'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
 const Background = () => {
   const shadows = useRef();
+
+  const snap = useSnapshot(state);
 
   return (
     <AccumulativeShadows
@@ -16,14 +21,14 @@ const Background = () => {
       position={[0, 0, -0.14]}
     >
       <RandomizedLight 
-        amount={4}
+        amount={3}
         radius={9}
         intensity={0.55}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight 
-        amount={4}
+        amount={3}
         radius={5}
         intensity={0.25}
         ambient={0.55}
